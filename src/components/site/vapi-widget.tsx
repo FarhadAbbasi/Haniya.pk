d9db7fd503ded6da4dc74027ab614e60a0383f66
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2025-11-18 13:41:08 +0500

    Completed foundational dashboard/admin functionalities like product addition/edit, adding/deleting variants and stocks etc

--- src/components/site/vapi-widget.tsx
+++ src/components/site/vapi-widget.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+// "use client"
 
-function vapiWidget() {
-    return (
-        <div>
-            <vapi-widget
-                mode="voice"
-                assistant-id="32894288-f0ee-497a-b70f-1995b0e9ae1e"
-                public-key="0c3f797a-7375-45e7-8834-6bc0bb87414a"
-            ></vapi-widget>
-            <script src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js" async type="text/javascript"></script>
+// import * as React from "react"
 
-        </div >
-    )
-}
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       "vapi-widget": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+//         mode?: string
+//         "assistant-id"?: string
+//         "public-key"?: string
+//       }
+//     }
+//   }
+// }
 
-export default vapiWidget
+
+
+
+// function VapiWidget() {
+//   return (
+//     <div>
+//       <vapi-widget
+//         mode="voice"
+//         assistant-id="32894288-f0ee-497a-b70f-1995b0e9ae1e"
+//         public-key="0c3f797a-7375-45e7-8834-6bc0bb87414a"
+//       ></vapi-widget>
+//       <script src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js" async type="text/javascript"></script>
+//     </div>
+//   )
+// }
+
+// export default VapiWidget
